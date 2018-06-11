675ce16197f34a7ad014adfaf0ac299b7a867b5c
jls2510 <jim@ping23.com>
2018-06-11 13:04:46 -0400

    Shared Static Queue Test

--- src/main/java/com/ping23/concurrency/SharedStaticQueueTest.java
+++ src/main/java/com/ping23/concurrency/SharedStaticQueueTest.java
@@ -1,27 +1,28 @@
 package com.ping23.concurrency;
 
-
-import java.util.concurrent.ConcurrentLinkedQueue;
-
 public class SharedStaticQueueTest {
-
-    public static ConcurrentLinkedQueue<String> myQueue = new ConcurrentLinkedQueue<String>();
 
     public static void main(String[] args) {
 
-
         for (int index = 0; index < 10; ++index) {
             final String hello = "Hello " + index;
+            System.out.println("adding to queue: " + hello);
             (new Thread(new Runnable() {
                 public void run() {
-                    //addToQueue("Hello");
-                    myQueue.add(hello);
+                    // addToQueue("Hello");
+                    SharedStaticQueue.myQueue.add(hello);
                 }
             })).start();
         }
 
-        while (myQueue.peek() != null) {
-            System.out.println(myQueue.poll());
+        try {
+            Thread.sleep(3000);
+        }
+        catch (Exception e) {
+        }
+
+        while (SharedStaticQueue.myQueue.peek() != null) {
+            System.out.println(SharedStaticQueue.myQueue.poll());
         }
     }
 
